d5ce97820d8a39587015949a723032b51473c405
Ilian <ilianmahzem@orange.fr>
2025-06-01 16:10:57 +0200

    first commit

---
+++ App/Config.php
@@ -0,0 +1,11 @@
+<?php
+namespace App;
+
+class Config
+{    
+    const SHOW_ERRORS = true;
+    const DB_HOST = 'mariadb';
+    const DB_NAME = 'mon_site';
+    const DB_USER = 'user';
+    const DB_PASSWORD = 'pass';
+}
